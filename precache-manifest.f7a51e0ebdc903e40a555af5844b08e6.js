0cc29caedd2de39e0b4e4f968fc4c5dfaa4851f4
Simon Walton <saw29@hotmail.co.uk>
2020-10-20 17:07:52 +0100

    Updates

---
+++ precache-manifest.f7a51e0ebdc903e40a555af5844b08e6.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bfc748acb13cc673ca299b31a9f022d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "452d1f4ed07f8265f323",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "f5c40baa2fb7cb0d3b41",
+    "url": "/static/css/main.7877ee83.chunk.css"
+  },
+  {
+    "revision": "452d1f4ed07f8265f323",
+    "url": "/static/js/2.3c9202c5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.3c9202c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5c40baa2fb7cb0d3b41",
+    "url": "/static/js/main.2dad62db.chunk.js"
+  },
+  {
+    "revision": "47a15619f3e06b2dbcf2",
+    "url": "/static/js/runtime-main.c05be1d9.js"
+  },
+  {
+    "revision": "4d97feaf1ee4a790ab1740c2b7e2934e",
+    "url": "/static/media/RHM1.4d97feaf.jpg"
+  },
+  {
+    "revision": "b49092e660fdcd785ca549c76308254d",
+    "url": "/static/media/RHM2.b49092e6.jpg"
+  },
+  {
+    "revision": "401ff02e59bd27c079d527b08d54e632",
+    "url": "/static/media/RHM3.401ff02e.jpg"
+  },
+  {
+    "revision": "9896f94a2eb05d8632b0d7c8f38c7cee",
+    "url": "/static/media/Rectangle 1.9896f94a.png"
+  },
+  {
+    "revision": "8d5361ef832b630a4164a77ece18ef7b",
+    "url": "/static/media/bedjevent.8d5361ef.jpg"
+  },
+  {
+    "revision": "719db299a528bfe55309aa9c49ce1eec",
+    "url": "/static/media/bedjlist.719db299.jpg"
+  },
+  {
+    "revision": "5b3ad267ae1286783f8006a0e5f18c5d",
+    "url": "/static/media/bedjmap.5b3ad267.jpg"
+  },
+  {
+    "revision": "f33569ab71651125ae453202c76102d5",
+    "url": "/static/media/bedjplay.f33569ab.jpg"
+  },
+  {
+    "revision": "fb9d0938d6bafd5b13c33b09cca582f2",
+    "url": "/static/media/c1.fb9d0938.jpg"
+  },
+  {
+    "revision": "a1017dcfde2c25c219935fc51cee18a9",
+    "url": "/static/media/c2.a1017dcf.jpg"
+  },
+  {
+    "revision": "21e9aa884ad124cbe3ddfdf415b66cc5",
+    "url": "/static/media/c3.21e9aa88.jpg"
+  },
+  {
+    "revision": "063709c858a5e27590ad5ded880ad90a",
+    "url": "/static/media/c4.063709c8.jpg"
+  },
+  {
+    "revision": "994d43f61c9adae64cb9ae566149e1e6",
+    "url": "/static/media/simon-headshot.994d43f6.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/simon-a-walton-dev/precache-manifest.99cea3708ed0791aeef0144b62ba9c61.js"
+  "/precache-manifest.f7a51e0ebdc903e40a555af5844b08e6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/simon-a-walton-dev/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
