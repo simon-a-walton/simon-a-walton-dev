9298cafa84d21663e605ec1cb615a2d906c81f02
Simon Walton <saw29@hotmail.co.uk>
2020-10-20 09:50:46 +0100

    Updates

---
+++ precache-manifest.8e6fc090d4bf4442951d2447c7bbdad5.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69d59791f72b15a0785480687c4337a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "def930cbd601f39a0e25",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "b5eef060c353f1339d17",
+    "url": "/static/css/main.97293791.chunk.css"
+  },
+  {
+    "revision": "def930cbd601f39a0e25",
+    "url": "/static/js/2.4bdc288d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.4bdc288d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5eef060c353f1339d17",
+    "url": "/static/js/main.aec1e0fc.chunk.js"
+  },
+  {
+    "revision": "47a15619f3e06b2dbcf2",
+    "url": "/static/js/runtime-main.c05be1d9.js"
+  },
+  {
+    "revision": "4d97feaf1ee4a790ab1740c2b7e2934e",
+    "url": "/static/media/RHM1.4d97feaf.jpg"
+  },
+  {
+    "revision": "b49092e660fdcd785ca549c76308254d",
+    "url": "/static/media/RHM2.b49092e6.jpg"
+  },
+  {
+    "revision": "401ff02e59bd27c079d527b08d54e632",
+    "url": "/static/media/RHM3.401ff02e.jpg"
+  },
+  {
+    "revision": "8d5361ef832b630a4164a77ece18ef7b",
+    "url": "/static/media/bedjevent.8d5361ef.jpg"
+  },
+  {
+    "revision": "719db299a528bfe55309aa9c49ce1eec",
+    "url": "/static/media/bedjlist.719db299.jpg"
+  },
+  {
+    "revision": "5b3ad267ae1286783f8006a0e5f18c5d",
+    "url": "/static/media/bedjmap.5b3ad267.jpg"
+  },
+  {
+    "revision": "f33569ab71651125ae453202c76102d5",
+    "url": "/static/media/bedjplay.f33569ab.jpg"
+  },
+  {
+    "revision": "fb9d0938d6bafd5b13c33b09cca582f2",
+    "url": "/static/media/c1.fb9d0938.jpg"
+  },
+  {
+    "revision": "a1017dcfde2c25c219935fc51cee18a9",
+    "url": "/static/media/c2.a1017dcf.jpg"
+  },
+  {
+    "revision": "21e9aa884ad124cbe3ddfdf415b66cc5",
+    "url": "/static/media/c3.21e9aa88.jpg"
+  },
+  {
+    "revision": "063709c858a5e27590ad5ded880ad90a",
+    "url": "/static/media/c4.063709c8.jpg"
+  }
+]);
