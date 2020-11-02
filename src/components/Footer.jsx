0fad0403f4966b347bf8e2813416a2f285734035
Simon Walton <saw29@hotmail.co.uk>
2020-11-02 09:47:16 +0000

    updated FOOTER component to have inline styling using Glamor and to use PropTypes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,38 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { css } from 'glamor';
 
-const Footer = () => {
-  return (
-    <>
-      <footer id="footer" className="p-3">
-        <div className="row text-center ">
-          <div className="col">
-            <a href='https://github.com/simon-a-walton'><i className="fab fa-github" /></a>
+const iconSizing = css({
+  fontSize: 'calc(16px + 1.2vh)'
+});
+
+const iconStyle = css({
+  color: 'white',
+  ':hover': {
+    color: 'black'
+  }
+});
+
+
+class Footer extends React.PureComponent {
+  static propsTypes = {
+    link: PropTypes.string,
+    iconName: PropTypes.string
+  };
+
+  render() {
+    return (
+      <>
+        <footer id="footer" className={`p-3 ${iconSizing}`}>
+          <div className="row text-center ">
+            <div className="col">
+              <a href={this.props.link}><i className={`fab fa-${this.props.iconName} ${iconStyle}`} /></a>
+            </div>
           </div>
-          <div className="col">
-            <a href='https://www.linkedin.com/in/simon-walton-36457323/'><i className="fab fa-linkedin" /></a>
-          </div>
-          <div className="col">
-            <a href="https://angel.co/u/simon-walton"><i className="fab fa-angellist" /></a>
-          </div>
-        </div>
-      </footer>
-    </>
-  )
+        </footer>
+      </>
+    )
+  }
 }
 
 export default Footer;
-
