d82334458687b43f18160caa24a96cd17aaac70d
Simon Walton <saw29@hotmail.co.uk>
2020-11-11 13:52:49 +0000

    updated github link to this porject's page

--- src/constants/LinkConstants.js
+++ src/constants/LinkConstants.js
@@ -1,5 +1,5 @@
 export const socialLinks = {
-  github: 'https://github.com/simon-a-walton',
+  github: 'https://github.com/simon-a-walton/simon-a-walton-dev/',
   linkedin: 'https://www.linkedin.com/in/simon-walton-36457323/',
   angellist: 'https://angel.co/u/simon-walton'
 };
